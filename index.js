9e3dd59b6f9d922350ada5e87e07d74f9874830b
mujahidina <mujahidabdi5070@gmail.com>
2024-01-08 19:23:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("userForm");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    const nameInput = document.getElementById("Name").value;
+    const emailInput = document.getElementById("email").value;
+    submitData(nameInput, emailInput);
+  });
+
+  function submitData(name, email) {
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        Name: name,
+        email: email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((user) => console.log(user))
+      .catch((error) => console.error("Error:", error));
+  }
+});
